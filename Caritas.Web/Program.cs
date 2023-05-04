2f3af141c0b14abe515ff17598ffdb1c2c2d84c6
Carlos DAgostino <dagsis@dagsis.com.ar>
2023-05-04 05:48:09 -0300

    Seguridad 3

--- Caritas.Web/Program.cs
+++ Caritas.Web/Program.cs
@@ -1,5 +1,6 @@
 using Caritas.Web.Persistence;
 using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
 using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,14 +9,37 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddPersistenceServices(builder.Configuration);
 
 //builder.Services.AddTransient<IAuthorizationHandler, AllowPrivateHandler>();
-builder.Services.AddAuthorization(opts =>
+builder.Services.AddAuthorization(options =>
 {
     //opts.AddPolicy("PrivateAccess", policy =>
     //{
     //    policy.AddRequirements(new AllowPrivatePolicy());
     //});
 
-    opts.AddPolicy("ViewClientes", policy => policy.RequireClaim("ViewClientes", "True"));
+    options.AddPolicy("ViewDashBoard", policy => policy.RequireClaim("ViewDashBoard", "True"));
+    options.AddPolicy("VerBotonDashBoard", policy => policy.RequireClaim("CreateDashBoard", "True"));
+    options.AddPolicy("EditDashBoard", policy => policy.RequireClaim("UpdateDashBoard", "True"));
+    options.AddPolicy("DeleteDashBoard", policy => policy.RequireClaim("DeleteDashBoard", "True"));
+
+    options.AddPolicy("ViewMaestros", policy => policy.RequireClaim("ViewMaestros", "True"));
+    options.AddPolicy("VerBotonMaestros", policy => policy.RequireClaim("CreateMaestros", "True"));
+    options.AddPolicy("EditMaestros", policy => policy.RequireClaim("UpdateMaestros", "True"));
+    options.AddPolicy("DeleteMaestros", policy => policy.RequireClaim("DeleteMaestros", "True"));
+
+    options.AddPolicy("ViewTablas", policy => policy.RequireClaim("ViewTablas", "True"));
+    options.AddPolicy("VerBotonTablas", policy => policy.RequireClaim("CreateTablas", "True"));
+    options.AddPolicy("EditTablas", policy => policy.RequireClaim("UpdateTablas", "True"));
+    options.AddPolicy("DeleteTablas", policy => policy.RequireClaim("DeleteTablas", "True"));
+
+    options.AddPolicy("ViewUsuarios", policy => policy.RequireClaim("ViewUsuarios", "True"));
+    options.AddPolicy("VerBotonUsuarios", policy => policy.RequireClaim("CreateUsuarios", "True"));
+    options.AddPolicy("EditUsuarios", policy => policy.RequireClaim("UpdateUsuarios", "True"));
+    options.AddPolicy("DeleteUsuarios", policy => policy.RequireClaim("DeleteUsuarios", "True"));
+
+    options.AddPolicy("ViewAdministracion", policy => policy.RequireClaim("ViewAdministracion", "True"));
+    options.AddPolicy("VerBotonAdministracion", policy => policy.RequireClaim("CreateAdministracion", "True"));
+    options.AddPolicy("EditAdministracion", policy => policy.RequireClaim("UpdateAdministracion", "True"));
+    options.AddPolicy("DeleteAdministracion", policy => policy.RequireClaim("DeleteAdministracion", "True"));
 
 });
 
