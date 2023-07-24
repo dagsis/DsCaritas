2f0dcde4e13ee0c4f8937c27811026e8ba9eb45c
Carlos DAgostino <dagsis@dagsis.com.ar>
2023-07-24 08:22:14 -0300

    Aviso7

--- Caritas.Web/Controllers/AvisoController.cs
+++ Caritas.Web/Controllers/AvisoController.cs
@@ -303,16 +303,95 @@ namespace Caritas.Web.Controllers
                                     {
                                         foster.Cell().Border(1).AlignCenter().Padding(2).Text("Fecha de vencimiento: 10 de septiembre de 2023").SemiBold();
                                         foster.Cell().Border(1).AlignRight().PaddingTop(2).PaddingRight(5).Text(total.ToString("N2")).SemiBold();
-                                    });
+                                    });                                  
+                                });
 
-                                    col.Item().Text("").FontSize(3);
+                                col.Item().Text("").FontSize(3);
+                                col.Item().Background(Colors.Grey.Lighten3).Padding(5)
+                                .Column(col =>
+                                {
+                                    col.Item().AlignCenter().Text("Si detecta que algunos de los períodos reclamados ya fue abonado por favor envie un e-mail a cobranzaspanteon@caritas.org.ar");
+                                });
+                                col.Item().Text("VALOR MES VENCIDO: NICHO $2.700 - URNA $2.100").SemiBold();
+                                col.Item().PaddingBottom(3).Text("Mes a vencer: NICHO $2.400 - URNA $1900").SemiBold();
+                                col.Item().PaddingBottom(3).Text("Se considera mes vencido desde 11 de cada mes").SemiBold();
+                                col.Item().Background(Colors.Orange.Lighten3).Text("  Recuerde que las cuotas no abonadas se calculan al valor vigente al momento del pago.").SemiBold();
+                                col.Item().PaddingTop(3).Text("FORMAS DE PAGO:").SemiBold();
 
-                                    col.Item().Background(Colors.Grey.Lighten3).Padding(5)
-                                    .Column(col =>
+                                col.Item().Row(row =>
+                                {
+                                    row.ConstantItem(94).Column(col =>
                                     {
-                                        col.Item().AlignCenter().Text("Si detecta que algunos de los períodos reclamados ya fue abonado por favor envie un e-mail a cobranzaspanteon@caritas.org.ar");
+                                        col.Item().PaddingTop(3).Text("1-Débito Automatico:").FontSize(9).SemiBold();
+
+                                    });
+                                    row.RelativeItem().Column(col =>
+                                    {
+                                        col.Item().PaddingTop(3).Text("Tarjetas de débito VISA: 5% de descuento sobre el valor de la cuota por un año.").FontSize(9);
+                                        col.Item().PaddingTop(3).Text("Tarjetas de crédito MASTERCARD O VISA: 10% de descuento sobre el valor de la cuota por un año.").FontSize(9);
+                                        col.Item().PaddingTop(3).Text("Para solicitar la adhesión debe escribir a: debitospanteon@caritasbsas.org.ar").SemiBold().FontSize(9);
                                     });
                                 });
+                                col.Item().Row(row =>
+                                {
+                                    row.ConstantItem(94).Column(col =>
+                                    {
+                                        col.Item().PaddingTop(3).Text("2-RAPIPAGO:").FontSize(9).SemiBold();
+
+                                    });
+                                    row.RelativeItem().Column(col =>
+                                    {
+                                        col.Item().PaddingTop(3).Text("Pagos sin facturas para Panteón Nuestra Señora de la Merced Código de Pago " + model[0].CPagoElectronico).FontSize(9);
+                                    });
+                                });
+                                col.Item().Row(row =>
+                                {
+                                    row.ConstantItem(94).Column(col =>
+                                    {
+                                        col.Item().PaddingTop(3).Text("3-PAGO FACIL:").FontSize(9).SemiBold();
+
+                                    });
+                                    row.RelativeItem().Column(col =>
+                                    {
+                                        col.Item().PaddingTop(3).Text("Pagos sin facturas para PAGOSPYME EXPRESS Código CYD" + model[0].CPagoElectronico).FontSize(9);
+                                    });
+                                });
+                                col.Item().Row(row =>
+                                {
+                                    row.ConstantItem(100).Column(col =>
+                                    {
+                                        col.Item().PaddingTop(3).Text("4-PAGO MIS CUENTAS:").FontSize(9).SemiBold();
+
+                                    });
+                                    row.RelativeItem().Column(col =>
+                                    {
+                                        col.Item().PaddingTop(3).Text("(Otros servicios) Panteón Nuestra Señora de la Merced Código de Pago " + model[0].CPagoElectronico).FontSize(9);
+                                    });
+                                });
+                                col.Item().Row(row =>
+                                {
+                                    row.ConstantItem(100).Column(col =>
+                                    {
+                                        col.Item().PaddingTop(3).Text("5-PAGAR (RED LINK):").FontSize(9).SemiBold();
+
+                                    });
+                                    row.RelativeItem().Column(col =>
+                                    {
+                                        col.Item().PaddingTop(3).Text("(Asociaciones y clubes) Panteón Nuestra Señora de la Merced Código de Pago " + model[0].CPagoElectronico).FontSize(9);
+                                    });
+                                });
+                                col.Item().PaddingTop(4).PaddingBottom(5).Text("No debe informar su pago, el mismo es identificado e informado por la compañía recaudadora.");
+
+                                col.Item().Row(row =>
+                                {
+                                    row.ConstantItem(300).Column(col =>
+                                    {
+                                        col.Item().Border(1).AlignMiddle().AlignCenter().Text("Próximo Vencimiento 10 de Diciembre de 2023");
+                                    });
+                                });
+
+                                col.Item().AlignRight().PaddingTop(3).PaddingRight(30).Text("La Administración").Underline().SemiBold();
+
                             });
                         });
 
